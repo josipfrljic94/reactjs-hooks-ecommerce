43201c801ac7cce551290c6851b21fdafa3a6ba2
josip frljic <josipfrljic77@gmail.com>
2020-04-27 02:20:17 +0200

    Updates

---
+++ precache-manifest.85be283447baf68cf7966d1d3836cc73.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013bd7df387470c78001ecba6ad9de96",
+    "url": "/reactjs-hooks-ecommerce/index.html"
+  },
+  {
+    "revision": "ed4fc6c489fdcd26748b",
+    "url": "/reactjs-hooks-ecommerce/static/css/main.af795da8.chunk.css"
+  },
+  {
+    "revision": "de26d91851ad895869b8",
+    "url": "/reactjs-hooks-ecommerce/static/js/2.70244bec.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactjs-hooks-ecommerce/static/js/2.70244bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed4fc6c489fdcd26748b",
+    "url": "/reactjs-hooks-ecommerce/static/js/main.ad447897.chunk.js"
+  },
+  {
+    "revision": "1c4584bcd4fa7689a0a2",
+    "url": "/reactjs-hooks-ecommerce/static/js/runtime-main.740405b6.js"
+  },
+  {
+    "revision": "36709b17c2285efe6e8f8fce9c9e4ce7",
+    "url": "/reactjs-hooks-ecommerce/static/media/bluza1.36709b17.jpg"
+  },
+  {
+    "revision": "4c14c73e608d2d6e6ffb0325acc9b32c",
+    "url": "/reactjs-hooks-ecommerce/static/media/bluza1p2.4c14c73e.jpg"
+  },
+  {
+    "revision": "575823ac24922fac20c806164aec7abc",
+    "url": "/reactjs-hooks-ecommerce/static/media/bluza2.575823ac.jpg"
+  },
+  {
+    "revision": "b09197295b2e37e880c03e1586937544",
+    "url": "/reactjs-hooks-ecommerce/static/media/bluza2p2.b0919729.jpg"
+  },
+  {
+    "revision": "f08cc8e31e76f77d90d832cddc026ca0",
+    "url": "/reactjs-hooks-ecommerce/static/media/bluza3.f08cc8e3.jpg"
+  },
+  {
+    "revision": "0fe42e2951a4dd4a8acef399a7828b41",
+    "url": "/reactjs-hooks-ecommerce/static/media/bluza3p2.0fe42e29.jpg"
+  },
+  {
+    "revision": "beccc2f7c46327ad3d4354cef119151d",
+    "url": "/reactjs-hooks-ecommerce/static/media/bluza4.beccc2f7.jpg"
+  },
+  {
+    "revision": "bdeb2e1410cba52c0e58eed17445d7b8",
+    "url": "/reactjs-hooks-ecommerce/static/media/bluza4p2.bdeb2e14.jpg"
+  },
+  {
+    "revision": "daeaa1ec8768aae4d8e67570db13a0ef",
+    "url": "/reactjs-hooks-ecommerce/static/media/bluza5.daeaa1ec.jpg"
+  },
+  {
+    "revision": "2d223d52f846b18726f4608f5c91fee1",
+    "url": "/reactjs-hooks-ecommerce/static/media/bluza5p2.2d223d52.jpg"
+  },
+  {
+    "revision": "7b674dce59b0cce8f5839af49a267b8a",
+    "url": "/reactjs-hooks-ecommerce/static/media/design.7b674dce.jpg"
+  },
+  {
+    "revision": "46996aa86ef287f6d2aba81dc821b6a0",
+    "url": "/reactjs-hooks-ecommerce/static/media/haljina1.46996aa8.jpg"
+  },
+  {
+    "revision": "74f84b2fc7bc3fe5b8f974f10b6faa30",
+    "url": "/reactjs-hooks-ecommerce/static/media/haljina1p2.74f84b2f.jpg"
+  },
+  {
+    "revision": "a1ba0aa13b4e40f17f84cd8a2554db53",
+    "url": "/reactjs-hooks-ecommerce/static/media/haljina2.a1ba0aa1.jpg"
+  },
+  {
+    "revision": "0f5f8b4508eb6baa594ee0acec59da58",
+    "url": "/reactjs-hooks-ecommerce/static/media/haljina2p2.0f5f8b45.jpg"
+  },
+  {
+    "revision": "96edaeb06aaccb053e228ee932f8a973",
+    "url": "/reactjs-hooks-ecommerce/static/media/haljina3.96edaeb0.jpg"
+  },
+  {
+    "revision": "2232495c71b4d2a400de64927fe03e1b",
+    "url": "/reactjs-hooks-ecommerce/static/media/haljina3p2.2232495c.jpg"
+  },
+  {
+    "revision": "271818479b7a4da8ee551ffaf884e3f9",
+    "url": "/reactjs-hooks-ecommerce/static/media/haljina4.27181847.jpg"
+  },
+  {
+    "revision": "81fa4fb2d39fc0712e56ce84553b4e80",
+    "url": "/reactjs-hooks-ecommerce/static/media/haljina4p2.81fa4fb2.jpg"
+  },
+  {
+    "revision": "3a9fed6ba322ef9304e0fd4b5284a0b7",
+    "url": "/reactjs-hooks-ecommerce/static/media/haljina5.3a9fed6b.jpg"
+  },
+  {
+    "revision": "47866f429a09e84e4d260246e40005c2",
+    "url": "/reactjs-hooks-ecommerce/static/media/haljina5p2.47866f42.jpg"
+  },
+  {
+    "revision": "649fa34ce5bbd704f196983616d3c194",
+    "url": "/reactjs-hooks-ecommerce/static/media/haljina6.649fa34c.jpg"
+  },
+  {
+    "revision": "14c6adb6cced7e830a53e32b17a0dbd3",
+    "url": "/reactjs-hooks-ecommerce/static/media/haljina6p2.14c6adb6.jpg"
+  },
+  {
+    "revision": "90c410e1c9fa755a8f3cd1c06987864b",
+    "url": "/reactjs-hooks-ecommerce/static/media/haljina7.90c410e1.jpg"
+  },
+  {
+    "revision": "27ee076824336f16912ad45926347817",
+    "url": "/reactjs-hooks-ecommerce/static/media/haljina7p2.27ee0768.jpg"
+  },
+  {
+    "revision": "34b2bea33540fa8da8d3687d603b4e6b",
+    "url": "/reactjs-hooks-ecommerce/static/media/homeprofil.34b2bea3.jpg"
+  },
+  {
+    "revision": "80ad8a57a23bdf01787b67317b2fddc0",
+    "url": "/reactjs-hooks-ecommerce/static/media/kosulja1.80ad8a57.jpg"
+  },
+  {
+    "revision": "afe98f3ec0b4f1dbbc9b78c6b5daa384",
+    "url": "/reactjs-hooks-ecommerce/static/media/kosulja1p2.afe98f3e.jpg"
+  },
+  {
+    "revision": "0f2d0b7dedaa2b0e341d431c33fc9f33",
+    "url": "/reactjs-hooks-ecommerce/static/media/kosulja2.0f2d0b7d.jpg"
+  },
+  {
+    "revision": "34a36ad36bf42fdb50819ed4d7a935dc",
+    "url": "/reactjs-hooks-ecommerce/static/media/kosulja2p2.34a36ad3.jpg"
+  },
+  {
+    "revision": "3292b4dcec0f5422beb4e6037c284832",
+    "url": "/reactjs-hooks-ecommerce/static/media/materials.3292b4dc.jpg"
+  },
+  {
+    "revision": "d688f54fe04593704d3e0edd2e75ebee",
+    "url": "/reactjs-hooks-ecommerce/static/media/sample.d688f54f.jpg"
+  },
+  {
+    "revision": "1ce6037f8bc12fc7ccf6af930fd0878a",
+    "url": "/reactjs-hooks-ecommerce/static/media/zip.1ce6037f.jpg"
+  }
+]);
